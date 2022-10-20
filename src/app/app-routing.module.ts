b9bfe6e6123fee781158a65e0c231f355f89c8ce
git_Nikos <nikosoikonomou@gmail.com>
2022-10-20 23:24:51 +0300

    Home Html, Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './home/home.component'
 import { SignInComponent } from './user/sign-in/sign-in.component';
 import { SignUpComponent } from './user/sign-up/sign-up.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-  {path: 'home', component : HomeComponent},
+  {path: 'home', component : HomeComponent,canActivate:[AuthGuard]},
   {
     path: 'signup', component : UserComponent,
     children : [{path: '', component: SignUpComponent}]

ac0250fac37bf8784affac4396475435e0fbe9d7
git_Nikos <nikosoikonomou@gmail.com>
2022-10-20 21:21:26 +0300

    Sign in, Sign up and Home Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component : HomeComponent},
+  {
+    path: 'signup', component : UserComponent,
+    children : [{path: '', component: SignUpComponent}]
+  },
+  {
+    path: 'login', component : UserComponent,
+    children : [{path: '', component: SignInComponent}]
+  },
+  {path: '', redirectTo:'/login', pathMatch: 'full'} //Default routing
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserComponent } from './user/user.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './route';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    UserComponent,
+    SignInComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -21,6 +29,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     HttpClientModule,
     ToastrModule.forRoot(),
     BrowserAnimationsModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
